44ebc2bd35b7369a2abc5c404d15518ff81e37fe
dcodeh <dcodyburrows@gmail.com>
2018-01-01 19:50:57 -0500

    Added header to test.c
    I didn't want it to feel left out

--- test.c
+++ test.c
@@ -1,3 +1,11 @@
+// test.c
+// @author Cody Burrows <dcodyburrows@gmail.com>
+//
+// Testing 123
+// is this thing on?
+// This program is used to test the functionality of darryl.
+//
+// ////////////////////////////////////////////////////////////////////////////
 #include <stdlib.h>
 #include <stdio.h>
 #include "darryl.h"
